b2d6d95b6117035179612a12ce2f9a0ecc081901
Pradip Chavda <chavdapradip48@gmail.com>
2023-10-25 16:17:01 +0530

    background-changer-app project added

---
+++ background-changer-app/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [backColor, setBackColor] = useState("olive");
+  const colorArray = ["Red", "Blue", "Green", "Yellow", "Orange", "Gray"];
+  document.body.style.backgroundColor = backColor;
+
+  function handleColor(color){
+    setBackColor(color);
+  }
+
+  return (
+    <div className="App" >
+      <h1 id="main-heading">Background Changer App</h1>
+      <div id="main-container">
+      {colorArray.map((color, index) => (
+          <ColorBox
+            key={index}
+            color={color.toLowerCase()}
+            label={color}
+            handler={() => handleColor(color.toLowerCase())}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ColorBox({color, label, handler}){
+  return (
+    <>
+    <botton 
+      className="color-box" 
+      onClick={()=> handler(color)} 
+      style = {{backgroundColor: color}}>
+      {label}
+    </botton>
+    </>
+  );
+}
